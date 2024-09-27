2bc0843f2289bc5d83151097ff138f38c44aec32
milcford <milcford@gmail.com>
2024-09-27 15:06:12 +0300

    upgrade stylish.py

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,10 +1,12 @@
 from diff import diff
 from parcer_file_extension import parse_file
 
+
 # ХЗ нужна тут эта сортировка или нет
 # total_keys = dict1 | dict2  # Объединяем ключи обоих словарей
 # sorted_keys = sorted(total_keys)
 def stylish(diff, depth=1):
+    # depth += 1
     result = []
     indent = depth * '....'
 
@@ -17,23 +19,29 @@ def stylish(diff, depth=1):
         if diff[key]['status'] == 'nested':
             # Добавляем только сам ключ двоеточие и отрытую фигурную скобку: common: {
             result.append(f'{indent}{key}: {{')
+            nested_diff = stylish(diff[key]['nested_changes'], depth= depth + 1)
+            result.append(nested_diff)
+            result.append(f'{indent}}}')
 
+        # Нигде не добавляются сами значения, подумать как их добавлять
+        # ПОКА ЧТО В ГОЛОВУ НИЧЕГО НЕ ПРИХОДИТ
         elif diff[key]['status'] == 'without_changed':
             result.append(f'{indent}{key}: {{')
+
         elif diff[key]['status'] == 'deleted':
             result.append(f'{indent[:-2]}- {key}: {{')
+
         elif diff[key]['status'] == 'added':
             result.append(f'{indent[:-2]}+ {key}: {{')
+
         elif diff[key]['status'] == 'changed':
             result.append(f'{indent[:-2]}- {key}: {{')
             result.append(f'{indent[:-2]}+ {key}: {{')
 
     print(result)
 
-
 dict1 = parse_file('/Users/milcford/hexlet/python-project-50/gendiff/tests/fixtures/file1.json')
 dict2 = parse_file('/Users/milcford/hexlet/python-project-50/gendiff/tests/fixtures/file2.json')
-
 
 diff = diff(dict1, dict2)
 
