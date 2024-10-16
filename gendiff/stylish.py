8dc8ee4b1d356867104446761b92905de403c63a
milcford <milcford@gmail.com>
2024-10-16 19:57:36 +0300

    upgrade project

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -5,29 +5,36 @@ from parcer_file_extension import parse_file
 # diff в данном случае это готовый словарь, depth это глубина которая будет увеличиваться
 # когда будет происходить рекурсивный вызов
 def stylish(diff, depth=1):
-
     result = []
     indent = depth * '....'
 
     for key in diff:
-        if diff[key]['status'] == 'added':
-            result.append(f'{indent[2:]}+.{key}: {diff[key]['value']}')
-        elif diff[key]['status'] == 'deleted':
-            result.append(f'{indent[2:]}-.{key}: {diff[key]['value']}')
-        elif diff[key]['status'] == 'changed':
-            result.append(f'{indent[2:]}-.{key}: {diff[key]['old_value']}')
-            result.append(f'{indent[2:]}-.{key}: {diff[key]['new_value']}')
-        elif diff[key]['status'] == 'without_changes':
-            result.append(f'{indent}{key}: {diff[key]['value']}')
-        if diff[key]['status'] == 'nested':
-            temp = stylish(diff[key]['nested_changes'], depth = depth + 1)
-            result.append(temp)
+        status = diff[key]['status']
+        if status == 'added':
+            result.append(f'{indent[2:]}+ {key}: {diff[key]["value"]}')
+        elif status == 'deleted':
+            result.append(f'{indent[2:]}- {key}: {diff[key]["value"]}')
+        elif status == 'changed':
+            result.append(f'{indent[2:]}- {key}: {diff[key]["old_value"]}')
+            result.append(f'{indent[2:]}+ {key}: {diff[key]["new_value"]}')
+        elif status == 'without_changes':
+            result.append(f'{indent}{key}: {diff[key]["value"]}')
+        elif status == 'nested':
+            result.append(f'{indent}{key}:')
+            nested_result = stylish(diff[key]['nested_changes'], depth + 1)
+            result.extend(nested_result)
 
-    print(result)
+    return result
+
+
 
 dict1 = parse_file('/Users/milcford/hexlet/python-project-50/gendiff/tests/fixtures/file1.json')
 dict2 = parse_file('/Users/milcford/hexlet/python-project-50/gendiff/tests/fixtures/file2.json')
 
 diff = diff(dict1, dict2)
+print(stylish(diff))
+# l = stylish(diff)
+#
+# for i in l:
+#     print(i)
 
-stylish(diff)
